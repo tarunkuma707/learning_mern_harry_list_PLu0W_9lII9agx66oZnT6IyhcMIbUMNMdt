c189aaef59852fe6b8158a00ba72fff9e70aa3b0
Tarun Kumar <tarunkuma07@gmail.com>
2025-05-01 11:05:38 +0530

    Understanding module default and not default in reactjs or Javascript

---
+++ module1.mjs
@@ -0,0 +1,2 @@
+import ui,{a,b,d} from "./module2.mjs";
+console.log(a,b,d,ui);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Tarun";
+const b = "Kumar";
+const c = "Aakash";
+const d = "Priyanka";
+
+export default c;
+export {a};
+export {b};
+export {d};
