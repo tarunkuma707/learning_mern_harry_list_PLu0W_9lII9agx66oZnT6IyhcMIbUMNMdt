14682423e6423a54bf947d616fa14ed24a7d00d0
Tarun Kumar <tarunkuma07@gmail.com>
2025-05-14 10:37:10 +0530

    Some formatting fixed

--- src/App.js
+++ src/App.js
@@ -1,47 +1,62 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import AboutUs from './components/AboutUs';
-import TextForm from './components/TextForm';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Child from './components/Child';
-import Childdata from './Childdata';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AboutUs from "./components/AboutUs";
+import TextForm from "./components/TextForm";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Child from "./components/Child";
+import Childdata from "./Childdata";
 
 function App() {
-  const [myData,setMyData]  = useState([]);
-  
-  const [mode,setMode] = useState('light'); /////////// Check Dark mode is enabled ////
-  
-  useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/posts")
-    .then((res)=>setMyData(res.data))
-  },[]);
-  const toggleMode = () => {
-    if(mode==='light')
-    {setMode('dark')
-      console.log("Here 1");
-      document.body.style.backgroundColor = 'grey'
-    }
-  else{
-      setMode('light')
-      document.body.style.backgroundColor = 'white'
-    }
+  const [myData, setMyData] = useState([]);
+
+  const [mode, setMode] = useState("light"); /////////// Check Dark mode is enabled ////
+  async function fetchData() {
+    let data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let dataJson = await data.json();
+    setMyData(dataJson);
   }
+  useEffect(() => {
+    //axios.get("https://jsonplaceholder.typicode.com/posts")
+    //.then((res)=>setMyData(res.data))
+    fetchData();
+  }, []);
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      console.log("Here 1");
+      document.body.style.backgroundColor = "grey";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
   const [dataFromChild, setDataFromChild] = useState("");
   const [inputdata, setInputdata] = useState("");
 
   function handleDataFromChild(data) {
     setDataFromChild(data);
   }
-  const getData =(newData) => setInputdata(newData);
+  const getData = (newData) => setInputdata(newData);
   return (
     <>
-      {myData.map((posts)=>{
-        return <div className="dd" style={{textAlign:'center'}}>
-              <h1>TITLE: {posts.title}</h1>
-              </div>
-      })};
-      <Navbar title ="TextUtils" aboutText="About Us" homeText="Home" mode={mode} toggleMode={toggleMode} />
+      <ul style={{ justifyItems: "center" }}>
+      {myData.map((posts) => {
+        return (
+          <li className="dd" key={posts.id} style={{ justifyItems: "center" }}>
+            TITLE: {posts.title}
+          </li>
+        );
+      })}
+      ;
+      </ul>
+      <Navbar
+        title="TextUtils"
+        aboutText="About Us"
+        homeText="Home"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <h1>Data from Child: {inputdata}</h1>
       <Childdata getData={getData} />
       <div>
@@ -53,7 +68,7 @@ function App() {
         <TextForm heading="Enter Text" mode={mode} />
         <AboutUs />
       </div>
-      </>
+    </>
   );
 }
 export default App;
